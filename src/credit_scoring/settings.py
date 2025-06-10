2fc088de9d1199b3aeaff9fc7599aa2096f7e650
Aleksander Siemiątkowski <127040227+siemiata@users.noreply.github.com>
2025-06-10 14:35:35 +0200

    Update settings.py

--- src/credit_scoring/settings.py
+++ src/credit_scoring/settings.py
@@ -1,9 +1,9 @@
 """Project settings. No Spark here."""
 
-# Brak hooków
+
 HOOKS = ()
 
-# Ustawienie loadera configu (jeśli faktycznie używasz niestandardowych wzorców)
+
 from kedro.config import OmegaConfigLoader
 
 CONFIG_LOADER_CLASS = OmegaConfigLoader
@@ -11,7 +11,7 @@ CONFIG_LOADER_ARGS = {
     "base_env": "base",
     "default_run_env": "local",
     "config_patterns": {
-        # Jeśli nie używasz spark*, możesz to też usunąć
+        
         "spark": ["spark*", "spark*/**"],
     },
 }
